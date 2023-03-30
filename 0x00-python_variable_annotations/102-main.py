8b4d1496ddb1e89ecf8fcfc550bbdb22f3ab788f
kalibetre <kalbetre@icloud.com>
2023-03-30 22:33:11 +0300

    fix static typing

---
+++ 0x00-python_variable_annotations/102-main.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""
+test module
+"""
+zoom_array = __import__('102-type_checking').zoom_array
+
+print(zoom_array.__annotations__)
---
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""
+102-type_checking module
+"""
+from typing import List
+
+
+def zoom_array(lst: List, factor: int = 2) -> List:
+    """
+    zoom in an array
+    """
+    zoomed_in: List = [item for item in lst for i in range(factor)]
+    return zoomed_in
+
+
+array = [12, 72, 91]
+
+zoom_2x = zoom_array(array)
+
+zoom_3x = zoom_array(array, 3)
