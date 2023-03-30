49164e717774df042b9251b6506287b530028ca3
kalibetre <kalbetre@icloud.com>
2023-03-30 22:11:21 +0300

    add docstring

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -6,6 +6,9 @@ from typing import Any, Sequence, Union
 
 
 def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
+    """
+    function that returns the first element of a list
+    """
     if lst:
         return lst[0]
     else:
