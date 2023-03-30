ba516c48dfa17f22c961e81619bc8d7f942f1fc8
kalibetre <kalbetre@icloud.com>
2023-03-30 21:34:57 +0300

    add doc str

--- 0x00-python_variable_annotations/8-make_multiplier.py
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -6,8 +6,14 @@ from typing import Callable
 
 
 def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """
+    creates a function that can be used as a multiplier
+    """
 
     def multiplier_func(num: float) -> float:
+        """
+        multiplies a number with the cached multiplier number
+        """
         return num * multiplier
 
     return multiplier_func
