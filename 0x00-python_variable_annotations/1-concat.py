efd1e60d9d624ae038231aee4a147350a160f22b
kalibetre <kalbetre@icloud.com>
2023-03-30 20:38:31 +0300

    add annotated concat function

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+1-concat module
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    concat function
+    """
+    return str1 + str2
---
+++ 0x00-python_variable_annotations/1-main.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+test module
+"""
+concat = __import__('1-concat').concat
+
+str1 = "egg"
+str2 = "shell"
+
+print(concat(str1, str2) == "{}{}".format(str1, str2))
+print(concat.__annotations__)
