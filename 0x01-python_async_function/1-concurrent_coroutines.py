e7701a39dc0d8b79832952cd78217c982ba5a782
kalibetre <kalbetre@icloud.com>
2023-04-03 13:54:10 +0300

    fix return type hint

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -3,11 +3,12 @@
 1-concurrent_coroutines module
 """
 import asyncio
+from typing import List
 
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> list:
+async def wait_n(n: int, max_delay: int) -> List[float]:
     """
     returns the list of all delays in ascending order
     """
