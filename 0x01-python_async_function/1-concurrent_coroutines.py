d8a7f577202504692305b3bbebe24578e61539e2
kalibetre <kalbetre@icloud.com>
2023-04-03 13:51:28 +0300

    fix function docstr

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -9,7 +9,7 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 
 async def wait_n(n: int, max_delay: int) -> list:
     """
-    returns the list of all the delays
+    returns the list of all delays in ascending order
     """
     c_routines = [wait_random(max_delay) for _ in range(n)]
     return [await delay for delay in asyncio.as_completed(c_routines)]
