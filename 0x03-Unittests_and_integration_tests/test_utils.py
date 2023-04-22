e80570fc30e47e15e1d2302538a498b9aea8102a
kalibetre <kalbetre@icloud.com>
2023-04-22 17:15:50 +0300

    test keyerror

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -45,3 +45,15 @@ class TestAccessNestedMap(unittest.TestCase):
         """Tests access_nested_map function with different inputs
         """
         self.assertEqual(access_nested_map(nested_map, path), expected)
+
+    @parameterized.expand([
+        ({}, ("a", )),
+        ({
+            "a": 1
+        }, ("a", "b")),
+    ])
+    def test_access_nested_map_exception(self, nested_map, path):
+        """Tests access_nested_map function with different inputs
+        """
+        with self.assertRaises(KeyError):
+            access_nested_map(nested_map, path)
