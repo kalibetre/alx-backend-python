9b3c6d172d635acdd34554809a9a97df2d93331d
kalibetre <kalbetre@icloud.com>
2023-04-22 17:38:38 +0300

    test memorize

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -2,12 +2,13 @@
 """A test module for utils module
 """
 import unittest
-from unittest.mock import patch
+from unittest.mock import PropertyMock, patch
 
 from parameterized import parameterized
 
 access_nested_map = __import__('utils').access_nested_map
 get_json = __import__('utils').get_json
+memoize = __import__('utils').memoize
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -80,3 +81,32 @@ class TestGetJson(unittest.TestCase):
             mock_get.return_value.json.return_value = test_payload
             self.assertEqual(get_json(test_url), test_payload)
             mock_get.assert_called_once_with(test_url)
+
+
+class TestMemoize(unittest.TestCase):
+    """A test class for memoize function
+    """
+
+    def test_memoize(self):
+        """Tests memoize function
+        """
+
+        class TestClass:
+            """A test class
+            """
+
+            def a_method(self):
+                """method to be tested"""
+                return 42
+
+            @memoize
+            def a_property(self):
+                """property to be tested"""
+                return self.a_method()
+
+        with patch.object(TestClass, 'a_method') as mock_method:
+            mock_method.return_value = 42
+            test_class = TestClass()
+            self.assertEqual(test_class.a_property, 42)
+            self.assertEqual(test_class.a_property, 42)
+            mock_method.assert_called_once()
